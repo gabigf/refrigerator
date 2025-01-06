7444ffa633df928a4ed789029099b81739f5124f
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2025-01-06 14:00:44 -0500

    Add test for create user

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,16 @@ from app.models import User, Item
 from app.api.endpoints import api
 from flask_login import LoginManager
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
 
     app.config["SECRET_KEY"] = 'thisismysecretkeywouldyouhaveguessedit'
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if testing:
+      app.config["TESTING"] = True
+      app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
 
     db.init_app(app)
 
