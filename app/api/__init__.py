8f25b8cb76ffb6b4c03fda140b4616dde2ac4095
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2025-03-31 23:34:06 -0400

    Register categories

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 from app.api.auth import auth
 from app.api.items import items
 from app.api.users import users
+from app.api.categories import categories
 
 def register_blueprints(app):
     app.register_blueprint(auth, url_prefix="/api")
     app.register_blueprint(items, url_prefix="/api")
     app.register_blueprint(users, url_prefix="/api")
+    app.register_blueprint(categories, url_prefix="/api")
